88e40db8b2ae445992d91f3a9f7409fcb20be0da
javayoyo <miyuzang613@gmail.com>
2023-05-31 16:56:53 +0900

    member_회원가입,목록_테스트및css설정

--- src/main/java/com/icia/member/repository/MemberRepository.java
+++ src/main/java/com/icia/member/repository/MemberRepository.java
@@ -3,5 +3,18 @@ package com.icia.member.repository;
 import com.icia.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+// 인터페이스 > '추상 메서드' 만 정의할 수 있음
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    /*
+        select => findBy
+        select * from member_table where member_email=?
+     */
+
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+
+    //   select * from member_table where member_email=? and member_password=?
+    Optional<MemberEntity> findByMemberEmailAndMemberPassword(String memberEmail, String memberPassword);
 }
