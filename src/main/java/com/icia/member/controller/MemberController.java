2e9559cc1f0d479acb4232e71970114a7bd2c7fc
javayoyo <miyuzang613@gmail.com>
2023-06-01 13:58:05 +0900

    member_선생님깃깃03_axios상세조회

--- src/main/java/com/icia/member/controller/MemberController.java
+++ src/main/java/com/icia/member/controller/MemberController.java
@@ -3,60 +3,82 @@ package com.icia.member.controller;
 import com.icia.member.dto.MemberDTO;
 import com.icia.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-
 @RequestMapping("/member")
 public class MemberController {
-
     private final MemberService memberService;
+
     @GetMapping("/save")
     public String saveForm() {
         return "memberPages/memberSave";
-
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         memberService.save(memberDTO);
-        return "memberPages/memberLogin";
+        return "index";
     }
 
     @GetMapping("/")
     public String findAll(Model model) {
         List<MemberDTO> memberDTOList = memberService.findAll();
-        model.addAttribute("memberList",memberDTOList);
+        model.addAttribute("memberList", memberDTOList);
         return "memberPages/memberList";
     }
 
     @GetMapping("/login")
     public String loginForm() {
         return "memberPages/memberLogin";
-
     }
 
     @PostMapping("/login")
     public String memberLogin(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         boolean loginResult = memberService.login(memberDTO);
-        if(loginResult) {
+        if (loginResult) {
             session.setAttribute("loginEmail", memberDTO.getMemberEmail());
             return "memberPages/memberMain";
-        }else {
+        } else {
             return "memberPages/memberLogin";
         }
-
-
-
     }
-    
+
+    @PostMapping("/login/axios")
+    public ResponseEntity memberLoginAxios(@RequestBody MemberDTO memberDTO, HttpSession session) throws Exception {
+        memberService.loginAxios(memberDTO);
+        session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/axios/{id}")
+    public ResponseEntity detailAxios(@PathVariable Long id) throws Exception {
+        MemberDTO memberDTO = memberService.findById(id);
+        return new ResponseEntity<>(memberDTO, HttpStatus.OK);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/icia/member/service/MemberService.java
+++ src/main/java/com/icia/member/service/MemberService.java
@@ -8,19 +8,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-
     private final MemberRepository memberRepository;
 
     public Long save(MemberDTO memberDTO) {
-
         MemberEntity memberEntity = MemberEntity.toSaveEntity(memberDTO);
         return memberRepository.save(memberEntity).getId();
-
     }
 
     public List<MemberDTO> findAll() {
@@ -34,16 +32,41 @@ public class MemberService {
 
     public boolean login(MemberDTO memberDTO) {
         Optional<MemberEntity> memberEntity =
-        memberRepository.findByMemberEmailAndMemberPassword(memberDTO.getMemberEmail(), memberDTO.getMemberPassword());
-        if(memberEntity.isPresent()) {
+                memberRepository.findByMemberEmailAndMemberPassword(memberDTO.getMemberEmail(), memberDTO.getMemberPassword());
+        if (memberEntity.isPresent()) {
             return true;
-        }else {
+        } else {
             return false;
         }
+    }
 
+    public void loginAxios(MemberDTO memberDTO) {
+        // chaining method (체이닝 메서드)
+        memberRepository.findByMemberEmailAndMemberPassword(memberDTO.getMemberEmail(), memberDTO.getMemberPassword())
+                .orElseThrow(() -> new NoSuchElementException("이메일 또는 비밀번호가 틀립니다"));
+    }
 
+    public MemberDTO findById(Long id) {
+        MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+        return MemberDTO.toDTO(memberEntity);
+//        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+//        if (optionalMemberEntity.isPresent()) {
+//            MemberEntity memberEntity = optionalMemberEntity.get();
+//            return MemberDTO.toDTO(memberEntity);
+//        } else {
+//            return null;
+//        }
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
