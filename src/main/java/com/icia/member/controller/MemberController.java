ebeaac916825ec5cb66e9c62a772cdddf390b1a5
javayoyo <miyuzang613@gmail.com>
2023-06-02 16:53:57 +0900

    member_로그아웃

--- src/main/java/com/icia/member/controller/MemberController.java
+++ src/main/java/com/icia/member/controller/MemberController.java
@@ -112,6 +112,11 @@ public class MemberController {
         }
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
 }
 
 
